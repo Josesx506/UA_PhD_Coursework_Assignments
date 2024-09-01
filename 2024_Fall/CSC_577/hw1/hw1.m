98a5c8e2ddb78423c4816295a908e081a5d5ed95
Josesx506 <45031412+Josesx506@users.noreply.github.com>
2024-08-31 17:00:28 -0700

    matlab scripts for CSC 577 hw1

--- 2024_Fall/CSC_577/hw1/hw1.m
+++ 2024_Fall/CSC_577/hw1/hw1.m
@@ -1,18 +1,28 @@
 function [num_questions] = hw1(infile)
-% This function reads in an image, performs some preprocessing
-% and outputs several images within the active directory. 
-% Additional text is also printed for some internal calculations
-% that were required.
+% This function reads in an image, performs some preprocessing and outputs 
+% several images within the active directory. 
+% Additional text is also printed for some internal calculations that were 
+% required. 
+% Most of the syntax is focused on manipulating image arrays, but the final
+% two questions are related to PCA analysis.
+% Input arg is a string path to the tent figure, and the function returns
+% the total number of questions that were answered.
 
 close all;
 clc;
 num_questions = 0;
 
+% Create an output dir to save the images
+if ~exist('output', 'dir')
+    mkdir('output');
+end
 
 
 %% Q1 - Q7
+figure;
 tent = imread(infile);
-imwrite(tent, "out.jpg");
+imshow(tent);
+imwrite(tent, "output/out.jpg");
 num_questions = num_questions + 7;
 
 %% Q8
@@ -45,14 +55,109 @@ disp(T);
 
 
 bw_tent = rgb2gray(tent);
-fprintf("B & W image dimensions are %d rows and %d columns.\n",size(bw_tent));
+fprintf("B & W image dimensions are %d rows and %d columns.\n\n\n",size(bw_tent));
 f1 = figure;
 imshow(bw_tent);
-imwrite(bw_tent, "bw_tent.jpg");
+imwrite(bw_tent, "output/bw_tent.jpg");
 
 num_questions = num_questions + 1;
 
-%%
+%% Q9
+figure;
+f2 = tiledlayout(2,2,'TileSpacing','Compact','Padding','Compact');
+
+nexttile;
+imshow(tent(:, :, 1));
+title('Red');
+
+nexttile;
+imshow(tent(:, :, 2));
+title('Green');
+
+nexttile;
+imshow(tent(:, :, 3)); 
+title('Blue');
+
+set(gca, 'LooseInset', get(gca, 'TightInset')); % Minimize whitespace
+exportgraphics(f2, 'output/tent_rgb_channels.png', 'Resolution', 200);
+
+flip_chans = zeros(size(tent));
+
+flip_chans(:, :, 1) = tent(:,:,2);
+flip_chans(:, :, 2) = tent(:,:,3);
+flip_chans(:, :, 3) = tent(:,:,1);
+figure;
+imshow(flip_chans);
+imwrite(flip_chans, "output/tent_flip_ch.png");
+
+num_questions = num_questions + 1;
+
+%% Q10
+doub_bw_tent = double(bw_tent) / 255;
+[nRows, nCols] = size(doub_bw_tent);
+
+for row = 5:5:nRows
+    for col = 5:5:nCols
+        doub_bw_tent(row, col) = 1;
+    end
+end
+
+f5 = figure;
+
+subplot(1,2,1);
+imagesc(doub_bw_tent);
+title('imagesc()');
+pbaspect([1 1 1]);
+
+subplot(1,2,2);
+imshow(doub_bw_tent);
+title('imshow()');
+
+set(gca, 'LooseInset', get(gca, 'TightInset'));
+exportgraphics(f5, 'output/tent_checker_bw_for_loop.png', 'Resolution', 200);
+
+num_questions = num_questions + 1;
+
+%% Q11
+
+redCh = reshape(tent(:, :, 1),[],1);
+grnCh = reshape(tent(:, :, 2),[],1);
+bluCh = reshape(tent(:, :, 3),[],1);
+
+figure;
+f6 = tiledlayout(2,2,'TileSpacing','Compact','Padding','Compact');
+
+nexttile;
+histogram(redCh, 20, 'FaceColor', 'r');
+title('Red');
+
+nexttile;
+histogram(grnCh, 20, 'FaceColor', 'g');
+title('Green');
+
+nexttile;
+histogram(bluCh, 20, 'FaceColor', 'b');
+title('Blue');
+
+exportgraphics(f6, 'output/tent_rgb_hist.png', 'Resolution', 200);
+
+num_questions = num_questions + 1;
+
+%% Q12
+
+f7 = figure;
+x_pi = linspace(-pi,pi,100);
+plot(x_pi,sin(x_pi), Color='r');
+hold on;
+plot(x_pi,cos(x_pi), Color='g');
+legend("sin","cos");
+hold off;
+
+exportgraphics(f7, 'output/trig_waves.png', 'Resolution', 200);
+
+num_questions = num_questions + 1;
+
+%% Q13
 % Define the matrix A
 A = [3 4 1; 2 -1 2; 1 1 -1];
 
@@ -67,6 +172,8 @@ X2 = linsolve(A,B);
 fprintf("Question 13: Playing with linear algebra\n")
 fprintf("Inverse:  x = %.4f, y = %.4f, z = %.4f\n", X1(1), X1(2), X1(3));
 fprintf("Linsolve: x = %.4f, y = %.4f, z = %.4f\n\n\n", X2(1), X2(2), X2(3));
+
+num_questions = num_questions + 1;
 
 %% Q14
 % Define the matrix A
@@ -96,6 +203,8 @@ error_magnitude = norm(error_vector);
 % Display the magnitude of the error vector
 fprintf("Magnitude of the error vector = %.4f\n\n\n", error_magnitude);
 
+num_questions = num_questions + 1;
+
 %% Q15
 % Step 1: Create a random 4x4 matrix R
 R = rand(4, 4);
@@ -123,8 +232,124 @@ fprintf("The matrix A*v./v is:\n");
 disp(Av_over_v);
 
 % Additionally, you can print out the eigenvalue to verify consistency
-fprintf("The eigenvalue k is: %.4f\n", k);
+fprintf("The eigenvalue k is: %.4f\n\n\n", k);
 
+num_questions = num_questions + 1;
+
+%% Q16
+doub_bw_tent = double(bw_tent) / 255;
+[nRows, nCols] = size(doub_bw_tent);
+
+doub_bw_tent(5:5:nRows, 5:5:nCols) = 0;
+
+f8 = figure;
+
+subplot(1,2,1);
+imagesc(doub_bw_tent);
+title('imagesc()');
+pbaspect([1 1 1]);
+
+subplot(1,2,2);
+imshow(doub_bw_tent);
+title('imshow()');
+
+set(gca, 'LooseInset', get(gca, 'TightInset'));
+exportgraphics(f8, 'output/tent_checker_bw_vectorized.png', 'Resolution', 200);
+
+doub_bw_tent = double(bw_tent) / 255;
+mask = find(doub_bw_tent>0.5);
+doub_bw_tent(mask) = 0;
+
+f9 = figure;
+imshow(doub_bw_tent);
+title('Pixels_>0.5');
+set(gca, 'LooseInset', get(gca, 'TightInset'));
+exportgraphics(f8, 'output/tent_bw_0.5_mask.png', 'Resolution', 200);
+
+num_questions = num_questions + 1;
+
+%% Q17
+pcaArr = readtable("pca.txt");
+pcaArr.Properties.VariableNames = ["x","y"];
+
+f10 = figure;
+scatter(pcaArr.x,pcaArr.y);
+title('PCA');xlabel('X');ylabel('Y');
+set(gca, 'LooseInset', get(gca, 'TightInset'));
+exportgraphics(f10, 'output/pca_raw.png', 'Resolution', 200);
+
+data = table2array(pcaArr);
+cov_mat = cov(data);
+fprintf("Question 17: PCA\n");
+fprintf("The original covariance matrix is \n");
+disp(cov_mat);
+fprintf("\n\n");
+
+% Shift origin to center and rotate matrix
+xCent = pcaArr.x - pcaArr.x(1);
+yCent = pcaArr.y - pcaArr.y(1);
+originCent = [xCent yCent];
+
+[eigvec, eigval] = eig(cov_mat);
+[sort_eigval, sort_idx] = sort(diag(eigval), 'descend');
+rotX = originCent * eigvec(:, sort_idx(1));
+rotY = originCent * eigvec(:, sort_idx(2));
+originRot = [rotX,rotY];
+
+% Shorter syntax
+% coeff = pca(originCent);
+% originRot = originCent * coeff;
+
+f11 = figure;
+scatter(originRot(:,1),originRot(:,2));
+title('PCA Origin Centered');xlabel('shift-rotation X');ylabel('shift-rotation Y');
+set(gca, 'LooseInset', get(gca, 'TightInset'));
+axis equal;
+exportgraphics(f11, 'output/pca_origin_cent.png', 'Resolution', 200);
+
+num_questions = num_questions + 1;
+
+%% Q18
+
+fprintf("Question 18: PCA II\n");
+centered_data = data - mean(data);
+
+% Calculate eigenvectors and eigenvalues
+[eigvec, eigval] = eig(cov(centered_data));
+
+fprintf("The eigenvector matrix is \n");
+disp(eigvec);
+
+% Verify orthogonality 
+ortho_check = eigvec' * eigvec;
+disp('Orthogonality Check (should be Identity Matrix):');
+disp(ortho_check);
+
+% Rotate data using eigenvectors
+transformed_data = centered_data * eigvec;
+
+% Plot transformed data
+f12 = figure;
+scatter(transformed_data(:, 1), transformed_data(:, 2));
+title('PCA Mean Centered');xlabel('PC1');ylabel('PC2');
+set(gca, 'LooseInset', get(gca, 'TightInset'));
+axis equal;
+exportgraphics(f12, 'output/pca_mean_cent.png', 'Resolution', 200);
+
+% Recompute covariance matrix for transformed data
+transformed_covariance = cov(transformed_data);
+disp('Transformed Covariance Matrix:');
+disp(transformed_covariance);
+
+% Sum of variances
+variance_before = sum(diag(cov(centered_data)));
+variance_after = sum(diag(cov(transformed_data)));
+T = table(variance_before, variance_after, ...
+    RowNames={'Variance Sum'}, VariableNames={'Before Transformation','After Transformation'});
+
+disp(T);
+
+num_questions = num_questions + 1;
 
 
 end
