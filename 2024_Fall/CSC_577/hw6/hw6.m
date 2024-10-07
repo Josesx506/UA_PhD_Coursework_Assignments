613dc456b337f464410d88f5a4854a22377198f0
Josesx506 <45031412+Josesx506@users.noreply.github.com>
2024-10-06 20:09:27 -0700

    CSC 577 hw6 part a

--- 2024_Fall/CSC_577/hw6/hw6.m
+++ 2024_Fall/CSC_577/hw6/hw6.m
@@ -1,4 +1,4 @@
-function [num_questions] = hw6()
+function [] = hw6()
 
 close all;
 clc;
@@ -12,76 +12,52 @@ end
 %% Part A
 fprintf("Part A\n")
 
-% Load the images (assuming you have 7 grayscale images, Img1, Img2, ..., Img7)
-I1 = imread('photometric_stereo_1.tiff');
-I2 = imread('photometric_stereo_2.tiff');
-I3 = imread('photometric_stereo_3.tiff');
-I4 = imread('photometric_stereo_4.tiff');
-I5 = imread('photometric_stereo_5.tiff');
-I6 = imread('photometric_stereo_6.tiff');
-I7 = imread('photometric_stereo_7.tiff');
-
-% Convert images to double
-I1 = double(rgb2gray(I1));
-I2 = double(rgb2gray(I2));
-I3 = double(rgb2gray(I3));
-I4 = double(rgb2gray(I4));
-I5 = double(rgb2gray(I5));
-I6 = double(rgb2gray(I6));
-I7 = double(rgb2gray(I7));
-
-% Stack images into 3D matrix
-[h, w] = size(I1); % Image dimensions
-I = zeros(h, w, 7);
-I(:,:,1) = I1;
-I(:,:,2) = I2;
-I(:,:,3) = I3;
-I(:,:,4) = I4;
-I(:,:,5) = I5;
-I(:,:,6) = I6;
-I(:,:,7) = I7;
-
-% Light directions (as provided in the question)
+% Load light directions
 L = readmatrix('light_directions.txt');
-
-% Initialize matrices for normals and albedo
-normals = zeros(h, w, 3);  % to store normals
-albedo = zeros(h, w);      % to store albedo
-
-% Solve for normals and albedo at each pixel
-for i = 1:h
-    for j = 1:w
-        % Extract intensity vector at pixel (i,j) across all images
-        I_vec = squeeze(I(i,j,:));  % 7x1 vector
-        
-        % Solve for the normal vector and albedo (Nx is the surface normal vector)
-        Nx = L \ I_vec;
-        
-        % The length of Nx corresponds to the albedo, and we normalize Nx to get the normal
-        albedo(i,j) = norm(Nx);  % Albedo is the magnitude of Nx
-        normals(i,j,:) = Nx / albedo(i,j);  % Normalized normal vector
-    end
-end
-
-% Rendering the canonical view (with a frontal light source in the direction [0, 0, 1])
-canonical_image = zeros(h, w);
-
-for i = 1:h
-    for j = 1:w
-        % The canonical light direction is [0, 0, 1], so we take the dot product with normal
-        N = squeeze(normals(i,j,:));
-        canonical_image(i,j) = max(0, N(3)) * 250; % We use max(0, N(3)) to ensure non-negative lighting
-    end
-end
+% Load the images
+[h, w, images, img_gray] = load_images('.');
+% Calculate the image normal
+normals = calculate_img_normals(h,w,L,img_gray);
+% Calculate the image albedo
+albedoImg = calculate_albedo(h,w,normals,L,images);
 
 % Display the canonical view image
 f1 = figure('visible','off');
-imshow(uint8(canonical_image), []);
-title('Canonical View (Uniform Albedo and Frontal Light)','FontSize',28);
+imshow(uint8(albedoImg*255), []);
+% box on; axis on;
+title('Canonical View (Uniform Albedo and Frontal Light)','FontSize',10);
 exportgraphics(f1, 'output/f1_canonical_view_of_images.png', 'Resolution', 200);
 
-num_questions = num_questions + 1;
+% Display the depth map
+x = 1:1:h;
+y = 1:1:w;
+r = get_depth_map(h,w,normals);
 
+f2 = figure('visible','off');
+surf(x,y,r,'EdgeColor', 'none'); colormap('parula');
+view(107, 64); % change the view direction
+title('Surface depth map from image normals','FontSize',10);
+% ax = gca;
+% ax.Interactions = [rotateInteraction dataTipInteraction];
+exportgraphics(f2, 'output/f2_surface_depth_mao.png', 'Resolution', 200);
+
+[X, Y] = meshgrid(x, y); % Create grid from x and y vectors
+Z = r;                   % Assuming r is a matrix corresponding to X and Y grid
+
+% Compute surface normals from the depth map
+[nx, ny, nz] = surfnorm(X, Y, Z);
+snormals = cat(3, nx, ny, nz);
+% Calculate the image albedo
+surf_albedoImg = calculate_albedo(h,w,snormals,L,images);
+
+
+f3 = figure('visible','off');
+imshow(uint8(surf_albedoImg*255), []);
+box on; axis on; 
+title('Canonical View (Uniform Albedo and Frontal Light)','FontSize',10);
+exportgraphics(f3, 'output/f3_reconstructed_image_from_surface_normals.png', 'Resolution', 200);
+
+num_questions = num_questions + 4;
 
 %% Part B
 fprintf("\nPart B\n")
@@ -89,4 +65,114 @@ fprintf("\nPart B\n")
 num_questions = num_questions + 1;
 
 
+end
+
+
+function [h, w, imgRGB, imgG] = load_images(directory)
+    % Get all files in the directory
+    files = dir(directory);
+    tiff_files = files(startsWith({files.name}, 'photometric_stereo') & endsWith({files.name}, '.tiff'));
+    [~, sortIndex] = sort({tiff_files.name});
+    tiff_files = tiff_files(sortIndex);
+
+    n_imgs = length(tiff_files);
+    imgRGB  = cell(n_imgs,1);
+    % Read the first file to get the image dimensions
+    I1 = im2double(rgb2gray(imread(tiff_files(1).name)));
+    [h, w] = size(I1);
+    imgG = zeros(h, w, n_imgs);
+
+    for n=1:n_imgs
+        fname = tiff_files(n).name;
+        img = imread(fname);
+        imgRGB{n} = img;
+        img = im2double(rgb2gray(img));
+        imgG(:,:,n) = img;
+    end
+end
+
+function [normals] = calculate_img_normals(h,w,LD,IMGG)
+    % Initialize matrices for normals
+    normals = zeros(h, w, 3);  % to store normals
+    
+    % Solve for normals at each pixel
+    for i = 1:h
+        for j = 1:w
+            % Extract intensity vector at pixel (i,j) across all images
+            I_vec = squeeze(IMGG(i,j,:));  % 7x1 vector
+            % Solve for the normal vector
+            G = (LD' * LD) \ (LD' * I_vec);
+            % Normalized normal vector
+            if norm(G) ~= 0
+                normals(i,j,:) = G / norm(G);
+            else
+                normals(i,j,:) = [0;0;0];
+            end
+        end
+    end
+end
+
+function [ch_albedo] = compute_channel_albedo(h, w, nImg, ch_int, J)
+    % Function to compute albedo for a single channel (R, G, or B)
+    % Inputs:
+    %   intensity: Intensity values for a single channel
+    %   J: Dot product between light directions and normals
+    
+    % Vectorized computation of albedo - flatten intensity and J to 2D arrays
+    flat_ints = reshape(ch_int, nImg, []);
+    J_flat = reshape(J, nImg, []);
+    
+    % Compute the albedo for each pixel
+    albedo_flat = sum(flat_ints .* J_flat) ./ sum(J_flat .* J_flat);
+    ch_albedo = reshape(albedo_flat, [h, w]);
+end
+
+function [albedoImg] = calculate_albedo(h,w,imgNorm,LD,imgs)
+    n_img = length(imgs);
+
+    rnorms = reshape(imgNorm, [], 3);
+    J = LD * rnorms';
+    J = reshape(J, [n_img, h, w]);
+
+    imgIntsR = zeros(n_img, h, w);
+    imgIntsG = zeros(n_img, h, w);
+    imgIntsB = zeros(n_img, h, w);
+    for n=1:n_img
+        imgn = imgs{n};
+        imgIntsR(n,:,:) = imgn(:,:,1);
+        imgIntsG(n,:,:) = imgn(:,:,2);
+        imgIntsB(n,:,:) = imgn(:,:,3);
+    end
+    
+    albedoImg = zeros(h, w, 3);
+    albedoImg(:, :, 1) = compute_channel_albedo(h,w,n_img,imgIntsR, J);
+    albedoImg(:, :, 2) = compute_channel_albedo(h,w,n_img,imgIntsG, J);
+    albedoImg(:, :, 3) = compute_channel_albedo(h,w,n_img,imgIntsB, J);
+
+    % Normalize the albedo image to be between 0 and 1
+    albedoImg = albedoImg / max(albedoImg(:));
+end
+
+function [r] = get_depth_map(h,w,normals)
+    Fx = size(h,w);
+    Fy = size(h,w);
+    for i=1:h
+        for j=1:w
+            normal = reshape(normals(i,j,:),1,3);
+            deriv = normal/normal(3);
+            Fx(i,j) = deriv(1);
+            Fy(i,j) = deriv(2);
+        end
+    end
+    
+    r=zeros(h,w);
+    for i=2:h
+        r(i,1)=r(i-1,1)+Fy(i,1);
+    end
+    
+    for i=2:h
+        for j=2:w
+            r(i,j)=r(i,j-1)+Fx(i,j);
+        end
+    end
 end
